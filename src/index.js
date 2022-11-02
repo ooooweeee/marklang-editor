96fca8b6c01a521c99601fbb4b4ac2563f4e5639
ooooweeee <you@example.com>
2022-11-02 23:17:48 +0800

    2ast解析结果

--- src/index.js
+++ src/index.js
@@ -2,5 +2,23 @@ import markdown2ast from './markdown2ast';
 
 const markdownString = `**Markdown 编辑器** - 要用优雅的姿势编辑 \`Markdown\``;
 
+/*
+[
+  {
+    "single": "**",
+    "tag": "b",
+    "value": "Markdown 编辑器"
+  },
+  {
+    "tag": "txt",
+    "value": " - 要用优雅的姿势编辑 "
+  },
+  {
+    "single": "`",
+    "tag": "span",
+    "value": "Markdown"
+  }
+]
+*/
 const ast = markdown2ast(markdownString);
 console.log(ast);
